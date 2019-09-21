3d332d968ac92588c47e6282aebaf7258e8da9b1
Chen Shaoju <oshirisu.red@gmail.com>
2019-09-21 12:24:02 +0800

    Frist commit
    Just upload these garbage.

---
+++ VNNBackupTunnelSwitcher/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(136, 55)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "切换"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "通道："
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "载入中"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "状态："
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(59, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 12)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "就绪"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(223, 90)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "VNN6通道切换器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ VNNBackupTunnelSwitcher/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO
+Imports System.Threading
+Public Class Form1
+    Function ReadLineWithNumberFrom(filePath As String, ByVal lineNumber As Integer) As String
+        Using file As New StreamReader(filePath)
+            ' Skip all preceding lines: '
+            For i As Integer = 1 To lineNumber - 1
+                If file.ReadLine() Is Nothing Then
+                    Throw New ArgumentOutOfRangeException("lineNumber")
+                End If
+            Next
+            ' Attempt to read the line you're interested in: '
+            Dim line As String = file.ReadLine()
+            If line Is Nothing Then
+                Throw New ArgumentOutOfRangeException("lineNumber")
+            End If
+            ' Succeded!
+            Return line
+        End Using
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ConfigLine As String = ReadLineWithNumberFrom(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg", 3)
+        Dim ConfigNumber As Integer = ConfigLine.Substring(ConfigLine.Length - 1)
+        If ConfigNumber = 0 Then
+            Label4.Text = "正在读取配置文件..."
+            Dim ConfigEdit As String = IO.File.ReadAllText(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg").Replace("onlyusetcp = 0", "onlyusetcp = 1")
+            Label4.Text = "正在修改配置文件..."
+            IO.File.WriteAllText(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg", ConfigEdit)
+            Label4.Text = "正在重启VNN6(停止中)..."
+            Process.Start("net", "stop vnn6csrv").WaitForExit()
+            Thread.Sleep(1000)
+            Label4.Text = "正在重启VNN6(启动中)..."
+            Process.Start("net", "start vnn6csrv").WaitForExit()
+            Thread.Sleep(1000)
+            Label4.Text = "成功！"
+            Label2.Text = "转发"
+        ElseIf ConfigNumber = 1 Then
+            Label4.Text = "正在读取配置文件..."
+            Dim ConfigEdit As String = IO.File.ReadAllText(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg").Replace("onlyusetcp = 1", "onlyusetcp = 0")
+            Label4.Text = "正在修改配置文件..."
+            IO.File.WriteAllText(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg", ConfigEdit)
+            Label4.Text = "正在重启VNN6(停止中)..."
+            Process.Start("net", "stop vnn6csrv").WaitForExit()
+            Thread.Sleep(1000)
+            Label4.Text = "正在重启VNN6(启动中)..."
+            Process.Start("net", "start vnn6csrv").WaitForExit()
+            Thread.Sleep(1000)
+            Label4.Text = "成功！"
+            Label2.Text = "直连"
+        Else
+            MsgBox("切换过程中遇到一个问题，请访问我们的网站 http://www.vnn.cn 以获取技术人员帮助。", MsgBoxStyle.Critical)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ConfigLine As String = ReadLineWithNumberFrom(System.Environment.GetEnvironmentVariable("ProgramFiles") + "\VNN6\core\settings.cfg", 3)
+        Dim ConfigNumber As Integer = ConfigLine.Substring(ConfigLine.Length - 1)
+        If ConfigNumber = 0 Then
+            Label2.Text = "直连"
+        ElseIf ConfigNumber = 1 Then
+            Label2.Text = "转发"
+        Else
+            Label2.Text = "读取错误"
+        End If
+    End Sub
+End Class
